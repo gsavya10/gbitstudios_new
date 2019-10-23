5141e45cac9535dd8b22ca35c8aae49e3854f46f
Savyasachi Gupta <gsavya10@gmail.com>
2019-10-23 18:59:51 +0530

    Revert "corrected base"
    This reverts commit 93b1cab2218f09d681b5949134fdb7ef39e21af2.

--- gbitstudios/authentication/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    response = {}
-    return render(request, 'home.html', response)
-    #return HttpResponse("<h1>Homepage</h1>")
